6894206a027be995a422214ca7738d4f5e11a0c1
Pavel <0x706a@gmail.com>
2024-03-04 23:34:07 +0200

    Added all threads to an arraylist

--- src/main/java/CounterThreads.java
+++ src/main/java/CounterThreads.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CounterThreads extends Thread {
+    // static map containing which threads have finished.
     private static final Map<String, Boolean> completionStatus = Collections.synchronizedMap(new HashMap<>());
+    // internal map to keep track of which completions we have acknowledged in this thread.
     private final Map<String, Boolean> acknowledgedCompletions = new HashMap<>();
     private final String notifiedOfOtherCompletion = "%s - Notified that other thread (%s) completed";
     private final String counting = "%s - Counting: %d";
@@ -19,26 +21,35 @@ public class CounterThreads extends Thread {
         super(name);
         this.startValue = startValue;
         this.endValue = endValue;
+        // state that this thread hasn't finished.
         completionStatus.put(name, false);
-        completionStatus.keySet().forEach(key -> acknowledgedCompletions.put(key, false));
+    }
+
+    // check for any threads which have finished, but we haven't yet acknowledged
+    private void checkOtherCompletion() {
+        completionStatus.forEach((threadName, isCompleted) -> {
+            if (!acknowledgedCompletions.containsKey(threadName)) {
+                acknowledgedCompletions.put(threadName, false);
+            }
+            if (isCompleted && !acknowledgedCompletions.get(threadName)) {
+                logger.info(notifiedOfOtherCompletion.formatted(getName(), threadName));
+                acknowledgedCompletions.put(threadName, true); // Mark this completion as acknowledged
+            }
+        });
+    }
+
+    // mark this thread as completed in static map
+    private void markCompletion() {
+        completionStatus.put(getName(), true);
+        logger.info(completed.formatted(getName()));
     }
 
     @Override
     public void run() {
         for (int i = startValue; i <= endValue; i++) {
-            // Check other threads' completion status
-            completionStatus.forEach((threadName, isCompleted) -> {
-                if (isCompleted && !acknowledgedCompletions.get(threadName)) {
-                    logger.info(notifiedOfOtherCompletion.formatted(getName(), threadName));
-                    acknowledgedCompletions.put(threadName, true); // Mark this completion as acknowledged
-                }
-            });
-
+            checkOtherCompletion();
             logger.info(counting.formatted(getName(), i));
         }
-
-        // Mark this thread as completed
-        completionStatus.put(getName(), true);
-        logger.info(completed.formatted(getName()));
+        markCompletion();
     }
 }
--- src/main/java/RunCounterThreads.java
+++ src/main/java/RunCounterThreads.java
@@ -1,27 +1,27 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RunCounterThreads {
     public static void main(String[] args) {
         final String allThreadsCompleted = "All threads completed their execution.";
-        final String thread1Name = "CounterThread-1";
-        final String thread2Name = "CounterThread-2";
-        final String thread3Name = "CounterThread-3";
 
         Logger logger = LogManager.getLogger(RunCounterThreads.class);
 
-        CounterThreads thread1 = new CounterThreads(thread1Name, 1, 3);
-        CounterThreads thread2 = new CounterThreads(thread2Name, 10, 15);
-        CounterThreads thread3 = new CounterThreads(thread3Name, 100, 110);
+        ArrayList<CounterThreads> threadList = new ArrayList<>(Arrays.asList(
+                new CounterThreads("CounterThread-1", 1, 5),
+                new CounterThreads("CounterThread-2", 100, 115),
+                new CounterThreads("CounterThread-3", 10, 11)
+        ));
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
+        threadList.forEach(Thread::start);
 
         try {
-            thread1.join();
-            thread2.join();
-            thread3.join();
+            for (CounterThreads thread : threadList) {
+                thread.join();
+            }
         } catch (InterruptedException e) {
             logger.trace(e);
         }
