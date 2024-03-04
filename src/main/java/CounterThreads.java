496f97e2a82b3515f3f79d11a537894935bcdae7
Pavel <0x706a@gmail.com>
2024-03-04 21:49:51 +0200

    Implemented CounterThreads

--- src/main/java/CounterThreads.java
+++ src/main/java/CounterThreads.java
@@ -1,2 +1,44 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class CounterThreads extends Thread {
+    private final String notifiedOfOtherCompletion = "%s - Notified that other thread completed";
+    private final String counting = "%s - Counting: %d";
+    private final String completed = "%s - Completed counting.";
+    private final int sleepMillis = 100;
+
+    private final int startValue;
+    private final int endValue;
+    private final AtomicBoolean myCompletionFlag;
+    private final AtomicBoolean otherCompletionFlag;
+    private boolean otherThreadAcknowledged = false;
+    static Logger logger = LogManager.getLogger(RunTwoCounterThreads.class);
+
+    public CounterThreads(String name, int startValue, int endValue, AtomicBoolean myCompletionFlag, AtomicBoolean otherCompletionFlag) {
+        super(name);
+        this.startValue = startValue;
+        this.endValue = endValue;
+        this.myCompletionFlag = myCompletionFlag;
+        this.otherCompletionFlag = otherCompletionFlag;
+    }
+
+    @Override
+    public void run() {
+        for (int i = startValue; i <= endValue; i++) {
+            if (otherCompletionFlag.get() && !otherThreadAcknowledged) {
+                logger.info(notifiedOfOtherCompletion.formatted(Thread.currentThread().getName()));
+                otherThreadAcknowledged = true;
+            }
+            logger.info(counting.formatted(getName(), i));
+            try {
+                Thread.sleep(sleepMillis);
+            } catch (InterruptedException e) {
+                logger.trace(e);
+            }
+        }
+        myCompletionFlag.set(true);
+        logger.info(completed.formatted(getName()));
+    }
 }
--- src/main/java/RunTwoCounterThreads.java
+++ src/main/java/RunTwoCounterThreads.java
@@ -1,3 +1,32 @@
-public class RunTwoCounterThreads {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class RunTwoCounterThreads {
+    public static void main(String[] args) {
+        final String bothThreadsCompleted = "Both threads completed their execution.";
+        final String thread1Name = "CounterThread-1";
+        final String thread2Name = "CounterThread-2";
+
+        Logger logger = LogManager.getLogger(RunTwoCounterThreads.class);
+
+        AtomicBoolean completionFlag1 = new AtomicBoolean(false);
+        AtomicBoolean completionFlag2 = new AtomicBoolean(false);
+
+        CounterThreads thread1 = new CounterThreads(thread1Name, 1, 5, completionFlag1, completionFlag2);
+        CounterThreads thread2 = new CounterThreads(thread2Name, 6, 15, completionFlag2, completionFlag1);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            logger.trace(e);
+        }
+
+        logger.info(bothThreadsCompleted);
+    }
 }
